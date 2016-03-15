49249271e970211d630e62116d998c52734baf1e
hoshiba10 <j_come_true@yahoo.co.jp>
2016-03-15 03:05:02 +0000

    add session and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'sessions/new'
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get     'signup', to: 'users#new'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
 end
