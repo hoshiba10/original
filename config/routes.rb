2e162ecb24ed0068580b811d6822b6a470e3c1a0
hoshiba10 <j_come_true@yahoo.co.jp>
2016-03-16 01:12:53 +0000

    fix desing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  
   get 'sessions/new'
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get     'signup', to: 'users#new'
-  resources :users
+  resources :users do
+    get :profile, on: :member
+  end
   resources :sessions, only: [:new, :create, :destroy]
   
 end
