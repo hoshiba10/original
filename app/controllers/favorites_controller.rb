08370f8adb831c52f693d1d0cdbcbed3f6c5b8e2
hoshiba10 <j_come_true@yahoo.co.jp>
2016-03-23 04:28:33 +0000

    add favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,8 +7,9 @@ class FavoritesController < ApplicationController
     end
         
     def destroy
-        @item = current_user.favorites.find_by(item_id: params[:favorite][:item_id])
-        current_user.unfavorite_item(@item)
+        @item = Item.find(params[:favorite][:item_id])
+        @item_fav = current_user.favorites.find_by(item_id: params[:favorite][:item_id])
+        current_user.unfavorite_item(@item_fav)
     end
 
 end
