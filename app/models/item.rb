2df30a4c9dfe630ed2b9e3de795350bf7b44721d
hoshiba10 <j_come_true@yahoo.co.jp>
2016-03-30 07:07:25 +0000

    fix desing

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,25 @@ class Item < ActiveRecord::Base
      belongs_to :user, foreign_key: "user_id"
      validates :user_id, presence: true
      mount_uploader :image, ImageUploader
-     validates :title, presence: true
-     validates :content, presence: true
-     validates :image, presence: true
+     #validates :title, presence: true
+     #validates :content, presence: true
+     #validates :image, presence: true
      
      has_many :favorite, :dependent => :destroy
      has_many :favorited_users, :through => :favorite, :source => :user
+     
+     validate :add_error_sample
+     
+     
+     def add_error_sample
+          if title.blank?
+               errors[:base] << "タイトル名を入力してください"
+          end
+          if content.blank?
+               errors[:base] << "内容を入力してください"
+          end
+          if image.blank?
+               errors[:base] << "画像をアップロードしてください"
+          end
+     end
 end
